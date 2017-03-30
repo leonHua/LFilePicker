fd40140f166fe435a80a613fec1a9090b1b00fbe
leon <open_leon@163.com>
2017-03-30 13:58:52 +0800

    update

--- lfilepickerlibrary/src/main/java/com/leon/lfilepickerlibrary/utils/Constant.java
+++ lfilepickerlibrary/src/main/java/com/leon/lfilepickerlibrary/utils/Constant.java
@@ -13,4 +13,6 @@ public class Constant {
     public static final int ICON_STYLE_YELLOW = 0;
     public static final int ICON_STYLE_BLUE = 1;
     public static final int ICON_STYLE_GREEN = 2;
+
+    public static String RESULT_INFO = "paths";
 }
